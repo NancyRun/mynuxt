c48a7e238176088a41db1281b5fdfc244bf1f6a8
jane <jane.mo@tvbc.com.cn>
2020-06-28 13:55:52 +0800

    add new project

---
+++ pages/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        mynuxt
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+        <nuxt-link class="button-grey" to="/about">about</nuxt-link>
+      </div>
+      <ul class="users">
+        <li v-for="user in users" :key="user.id">
+          <nuxt-link :to="'/users/' + user.id">{{ user.name }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+    return { users: data }
+  },
+  head: {
+    title: 'Home page 🚀',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home page description',
+      },
+    ],
+    noscript: [
+      {
+        innerHTML: 'Body No Scripts',
+        body: true,
+      },
+    ],
+    script: [
+      { src: '/head.js' },
+      // Supported since 1.0
+      { src: '/body.js', body: true },
+      { src: '/defer.js', defer: '' },
+    ],
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
